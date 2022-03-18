9bccfdbb23c85d1214c881cba71a8f36aa748b04
Camilo <cavs1010@gmail.com>
2022-03-18 16:15:41 +1000

    initial

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Form } from "semantic-ui-react";
 
 export const ContactForm = ({
   name,
@@ -11,14 +12,23 @@ export const ContactForm = ({
 }) => {
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Name:</label>
-      <input type="text" value={name} onChange={(event) => setName(event.target.value)}/>
-      <label>Phone:</label>
-      <input type="text" value={phone} onChange={(event) => setPhone(event.target.value)} pattern="[0-9]+"/>
-      <label>Email:</label>
-      <input type="text" value={email} onChange={(event) => setEmail(event.target.value)}/>
-      <input type='submit'/>
-    </form>
+    <Form>
+      <Form.Field>
+        <label>Name:</label>
+        <input type="text" value={name} onChange={(event) => setName(event.target.value)}/>
+      </Form.Field>
+      <Form.Field>
+        <label>Phone:</label>
+        <input type="text" value={phone} onChange={(event) => setPhone(event.target.value)} pattern="[0-9]+"/>
+      </Form.Field>
+    </Form>
+    // <form onSubmit={handleSubmit}>
+
+    //   <label>Phone:</label>
+    //   <input type="text" value={phone} onChange={(event) => setPhone(event.target.value)} pattern="[0-9]+"/>
+    //   <label>Email:</label>
+    //   <input type="text" value={email} onChange={(event) => setEmail(event.target.value)}/>
+    //   <input type='submit'/>
+    // </form>
   );
 };
